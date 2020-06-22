590946d1f29f5b3d6e3c6ca75a7adb942444a84e
AneshE <mailanesh@gmail.com>
2020-06-22 19:26:18 +0530

    completed normal questions about enron db

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,33 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
-
-
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print(len(enron_data))
+print (len(enron_data["SKILLING JEFFREY K"].keys()))
+names_file = open(r"../final_project/poi_names.txt", "r")
+names = names_file.read().upper()
+sum = 0
+poi = 0
+salary = 0
+email = 0
+for (key, value) in enron_data.items():
+    #print value["salary"]
+    if value["poi"]:
+        sum+=1
+        if (names.find(key.split(' ')[1].strip())) > 0 :
+            #print key
+            poi+=1
+    if value["salary"] != "NaN":
+        salary+=1
+    if value["email_address"] != "NaN":
+        email +=1
+print sum
+print poi
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print salary
+print email
