dbeb9c8e25528fcd5b6263bc12e2468041b07ecb
AneshE <mailanesh@gmail.com>
2020-06-22 11:24:22 +0530

    completed analysing kneighbors random forest and adaboost classifiers

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,14 +24,29 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+#plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import KNeighborsClassifier
+kClassifier = KNeighborsClassifier(n_neighbors=8)
+#kClassifier.fit(features_train, labels_train)
+#print kClassifier.score(features_test, labels_test) # 0.944
 
+from sklearn.ensemble import RandomForestClassifier
+rfClassifier = RandomForestClassifier(min_samples_split=5)
+#rfClassifier.fit(features_train, labels_train)
+#print rfClassifier.score(features_test, labels_test) # 0.92
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+svc = SVC(kernel='linear', probability=True)
+abClassifier = AdaBoostClassifier(n_estimators=15, learning_rate=1)
+abClassifier.fit(features_train, labels_train)
+print abClassifier.score(features_test, labels_test) #0.928
 
 
 
