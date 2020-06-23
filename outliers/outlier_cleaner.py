c9a3043241521236b11415be5777a225a563cdee
AneshE <mailanesh@gmail.com>
2020-06-23 14:37:10 +0530

    Cleaning outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,14 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    errors=[]
+    print (zip(ages, predictions, net_worths))
+    for prediction, net_worth in zip(predictions, net_worths):
+        errors.append(prediction - net_worth)
+    
+    cleaned_data=zip(ages, net_worths, errors)
+    samples = int(len(cleaned_data)*0.9)
+    cleaned_data.sort(key=lambda tup: (tup[2]))
 
     
-    return cleaned_data
-
+    return cleaned_data[:samples]
